ef4f00761efdfe59761d7605f65d4167e170b605
Derek Nicol <1420397+derekn@users.noreply.github.com>
2020-08-27 11:04:11 -0500

    minor mod and requirements update

--- blshift/__main__.py
+++ blshift/__main__.py
@@ -8,7 +8,7 @@ from tempfile import gettempdir
 import click as cl
 from diskcache import Cache
 
-from . import Shift, __url__, __version__
+from . import Shift, __copyright__, __url__, __version__
 
 
 def redeem_code_init(shift):
@@ -19,14 +19,14 @@ def redeem_code(code):
 	return code, success, msg
 
 @cl.command(no_args_is_help=True, epilog=__url__)
-@cl.version_option(version=__version__, message=f"%(prog)s version %(version)s, Copyright 2020 Derek Nicol\n{__url__}")
+@cl.version_option(version=__version__, message=f"%(prog)s version %(version)s, {__copyright__}\n{__url__}")
 @cl.option('-u', '--user', required=True, help='shift username.', envvar='SHIFT_USERNAME')
 @cl.option('-p', '--pass', 'password', required=True, help='shift password.', envvar='SHIFT_PASSWORD')
 @cl.option('-l', '--platform', required=True, help='redemption platform.', envvar='SHIFT_PLATFORM',
            type=cl.Choice([x.name for x in Shift.Platforms], case_sensitive=False))
 @cl.option('-c', '--code', 'codes', help='redeem single shift code, can be used multiple times.', multiple=True)
 @cl.option('--no-cache', is_flag=True, default=False, help='disable shift code caching.')
-def main(user, password, platform, codes=None, no_cache=False):
+def main(user, password, platform, codes, no_cache):
 	"""redeem all active or individual shift codes for Borderlands"""
 
 	no_cache = True if codes else no_cache
